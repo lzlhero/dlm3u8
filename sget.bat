3230da51411504818102e2b32fc75a74f235f47e
lzlhero <lzlhero@gmail.com>
2026-01-20 09:36:54 +0800

    change sget concurrent downloads number

--- sget.bat
+++ sget.bat
@@ -1,4 +1,4 @@
 :: Copyright (c) 2025 lzlhero
 :: Licensed under the GNU General Public License v3.0 (GPL-3.0)
 
-@aria2c --max-concurrent-downloads=1 --split=1 %*
+@aria2c --max-concurrent-downloads=6 --split=1 %*
